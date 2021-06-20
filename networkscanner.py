2e7a2a3a5c0abb0d012eb036609d8147e6b56304
reselection <danieldcwverlinden@gmail.com>
2021-06-20 19:08:22 +0200

    remade the directory

---
+++ networkscanner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from scapy.all import ARP, Ether, srp
+
+#Scanner that neatly outputs all online devices
+
+scanfile = "scan.txt"
+target = "192.168.2.254/24"
+arp = ARP(pdst=target)
+#Creates ARP packet
+
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+#Create ether broadcast packet
+
+packet = ether/arp
+result = srp(packet, timeout=3)[0]
+
+clients = []
+for send, recieved in result:
+    #append IP and mac address to clients list per response
+    clients.append({"ip": recieved.psrc, "mac": recieved.hwsrc})
+
+print("Found devices:")
+print("IP" + " "*18+"MAC")
+
+
+for client in clients:
+    print(f"{client['ip']} : {client['mac']}")
