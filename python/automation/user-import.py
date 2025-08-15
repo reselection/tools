1d36eaa219a09ffb8e162ff7e38148efadc88eb4
Reselection <danieldcwverlinden@gmail.com>
2025-08-15 13:00:22 +0200

    added func for file import and testing create users

---
+++ python/automation/user-import.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import csv
+
+## Checks the OS, this script will only work on Linux.
+from sys import platform
+if platform in('linux','linux2'):
+    pass
+elif platform not in ('linux','linux2'):
+    print(f'This script does not support',platform)
+    #exit()
+
+## Script for csv user creation, adds to group, sets shell & home
+## Linux
+
+def import_file(user_file):
+    ## Main function
+    ## Prepares and opens the file for data manipulation.
+    ## Sends data over to other functions depending on input supplied
+    with open(user_file) as data:
+        userdata = csv.DictReader(data)
+        rows = list(userdata)
+ 
+    option = input("Add or remove users? (add / remove) ").lower()
+    if option in('add','a'):
+        create_users(rows)
+    elif option in ('remove','r'):
+        delete_users(rows)
+
+
+def create_users(users):
+    verbose = input("Verbose? Y/n: ").lower()
+
+    if verbose == 'y':
+        for user in users:
+            subprocess.run(['echo', f"{user['username']}"])
+
+def delete_users(*args):
+    pass
+
+def add_users_group(*args):
+    pass
+
+def remove_users_group(*args):
+    pass
+
+import_file('users.csv')
